078e5e0f2c85e0f08fb339d6eec79e8eafd28631
VarunGoud08 <varungoud5520@gmail.com>
2024-03-29 10:00:07 +0530

    -first

--- src/main/java/com/streaming/demo/Movies/Controller/MoviesController.java
+++ src/main/java/com/streaming/demo/Movies/Controller/MoviesController.java
@@ -1,2 +1,17 @@
-package com.streaming.demo.Movies.Controller;public class MoviesController {
+package com.streaming.demo.Movies.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MoviesController {
+
+    @GetMapping("/req")
+    public ResponseEntity<String> requestMovies(){
+        return new ResponseEntity<>("Request Movie will be added in 15 Days", HttpStatus.OK);
+    }
 }
